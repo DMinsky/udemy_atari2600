9e81e518ed563ffd042b0f4e241356c17d325ec0
Dmitry Minsky <me@dminsky.com>
2021-05-23 18:41:21 +0300

    Add building bat scripts.

---
+++ build.bat
@@ -0,0 +1,1 @@
+dasm %1.dasm -f3 -v0 -l%1.lst -s%1.sym -o%1.rom -I"C:\Program Files\Dasm\machines\atari2600"
---
+++ run.bat
@@ -0,0 +1,2 @@
+dasm %1.dasm -f3 -v0 -o%1.rom -I"C:\Program Files\Dasm\machines\atari2600"
+stella %1.rom
